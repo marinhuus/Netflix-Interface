f8a2fe8270d0b32499001c7ca3667050a31b09a9
Marinhuus <marinhuus@gmail.com>
2021-09-08 02:01:30 -0300

    Netflix-Inferface

---
+++ README.md
@@ -0,0 +1,3 @@
+# Netflix-Inferface
+
+Projeto de um desafio dentro da plataforma da Digital Innovation One, replicar a interface do Netflix usando todo conhecimento que foi passado nas aulas e por fim da sua cara a essa Interface, alem de funcionalidades aos botões e um pouco de refino.
---
+++ js/owl/setup.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function assistiragora(){
+    window.open("https://www.youtube.com/watch?v=2iKZmRR9AR4");
+}
+
+function maisinfo(){
+    window.open("https://pt.wikipedia.org/wiki/The_Office_(Estados_Unidos)")
+}
